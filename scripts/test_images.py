9550e80c09de1acaab9643de7be2a552bfcb1c75
Carmoucha <157990722+Carmoucha@users.noreply.github.com>
2024-05-27 15:12:44 -0400

    Create test_images.py

---
+++ scripts/test_images.py
@@ -0,0 +1,23 @@
+# scripts/test_images.py
+
+# Select 100 random test images
+
+import os
+import random
+import shutil
+
+# PUT YOUR PATH
+input_dir = '/data/labeled'
+output_dir = '/data/test'
+num_images = 100
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+all_images = [f for f in os.listdir(input_dir) if f.endswith('.jpg') or f.endswith('.png')]
+selected_images = random.sample(all_images, num_images)
+
+for image in selected_images:
+    shutil.move(os.path.join(input_dir, image), os.path.join(output_dir, image))
+
+print(f"Selected {num_images} images from {input_dir} and moved to {output_dir}.")
