98918606cbefe54df0dcc9335f5f22505c49ac82
Carmoucha <157990722+Carmoucha@users.noreply.github.com>
2024-05-27 16:20:56 -0400

    Update label_images.py

--- scripts/label_images.py
+++ scripts/label_images.py
@@ -1,24 +1,40 @@
-# scripts/label_images.py
-
 import os
 import json
 import shutil
 
-#PUT YOUR PATH
-input_dir = '../data/'
-output_dir = '../data/labeled/'
+# Input directories
+input_dirs = {
+    'happy_faces': '../data/raw/happy_faces',
+    'neutral_faces': '../data/raw/neutral_faces',
+    'focused_faces': '../data/raw/focused_faces',
+    'angry_faces': '../data/raw/angry_faces'
+}
+
+# Output directories
+output_dirs = {
+    'happy_faces': '../data/labeled/happy_faces',
+    'neutral_faces': '../data/labeled/neutral_faces',
+    'focused_faces': '../data/labeled/focused_faces',
+    'angry_faces': '../data/raw/angry_faces'
+}
+
+# Labels
 labels = {}
 
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+for face_type, input_dir in input_dirs.items():
+    output_dir = output_dirs[face_type]
 
-for filename in os.listdir(input_dir):
-    if filename.endswith(".jpg") or filename.endswith(".png"):
-        labels[filename] = 'happy'
-        shutil.move(os.path.join(input_dir, filename), os.path.join(output_dir, filename))
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            labels[filename] = face_type
+            shutil.copy(os.path.join(input_dir, filename), os.path.join(output_dir, filename))
 
 # Save labels to a JSON file
-with open(os.path.join(output_dir, 'labels.json'), 'w') as f:
+with open('../data/labeled/labels.json', 'w') as f:
     json.dump(labels, f)
 
 print("Images labeled successfully.")
+
